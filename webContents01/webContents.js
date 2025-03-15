61ce7aded89d769ac0fd62470ad740bb621cafc3
kim jae eun <rlawodms95@naver.com>
2025-03-15 21:04:40 +0900

    web-snippet - css 애니메이션 설정 / js 뉴스 리스트 활성상태 설정

--- webContents01/webContents.js
+++ webContents01/webContents.js
@@ -1,5 +1,10 @@
-let actionList = document.querySelector(".guri-news-menu > ul > li");
+let actionList = document.querySelectorAll(".guri-news-menu > ul > li");
 
-actionList.addEventListener("click", function(){
-    this.classList.add("action");
-})
+actionList.forEach(function(item) {
+    item.addEventListener("click", function(){ // 버튼을 클릭 했을 시
+        actionList.forEach(function(el){    // actionList를 순회해서,
+            el.classList.remove("action");  // action클래스명을 모든요소에서 제거
+        });
+        this.classList.add("action");   // 그리고 action클래스명 추가
+    })
+});
